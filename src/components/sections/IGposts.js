6384e244c643585dfd435d6838af263d96db9bfa
murphylee10 <murphylee2004@gmail.com>
2023-12-04 23:26:03 -0500

    ig stuff

--- src/components/sections/IGposts.js
+++ src/components/sections/IGposts.js
@@ -1,183 +1,87 @@
-import React, { useEffect, useState } from 'react';
-import InstagramEmbed from 'react-instagram-embed'
-import styled from 'styled-components';
+import { graphql, useStaticQuery } from "gatsby";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Post = () => {
+  const posts = useStaticQuery(
+    graphql`
+      query {
+        allInstaNode(
+          sort: { fields: timestamp, order: DESC }
+          filter: { mediaType: { ne: "VIDEO" } }
+        ) {
+          edges {
+            node {
+              id
+              permalink
+              timestamp
+              caption
+              mediaType
+              localFile {
+                publicURL
+              }
+            }
+          }
+        }
+      }
+    `
+  ).allInstaNode.edges;
+
+  return (
+    <div>
+      <Feed>
+        {posts.map(({ node }) => {
+          const { id, caption, permalink, localFile, timestamp, mediaType } =
+            node;
+
+          return (
+            <div className="post" key={id}>
+              <a href={permalink}>
+                <img
+                  className="image"
+                  src={localFile.publicURL}
+                  alt={caption}
+                />
+              </a>
+
+              {/* <div className="caption">
+              <p>{caption}</p>
+              <a href={permalink} target="_blank" rel="noopener noreferrer">
+                View on Instagram
+              </a>
+            </div> */}
+            </div>
+          );
+        })}
+      </Feed>
+    </div>
+  );
+};
+export default Post;
 
 export const Feed = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 
-    .post {
-        margin: irem;
-        width: calc(25% - 2rem);
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        transition: transform 0.3s ease-in-out;
+  .post {
+    margin: irem;
+    width: calc(25% - 2rem);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease-in-out;
 
-        .image {
-            width: 100%;
-            height: auto;
-            display: block;
-        }
-
-        &:hover {
-            transform: scale(1.05);
-        }
-
-        .caption {
-            padding: irem;
-        }
+    .image {
+      width: 100%;
+      height: auto;
+      display: block;
     }
-`;
 
+    &:hover {
+      transform: scale(1.05);
+    }
 
-class Post extends React.Component {
-    render() {
-      // const posts = this.props.data.allInstaNode.edges;
-
-      const { data } = this.props;
-
-      // Filter the node data to avoid null objects
-      const posts = data.allInstaNode.edges.filter(
-        ({ node }) => node.localFile && node.localFile.childImageSharp && node.mediaType !== 'VIDEO'
-      );
-  
-      return (
-        <Feed>
-            {posts.map(({ node }) => {
-                const { id, caption, permalink, localFile, timestamp, mediaType } = node;
-
-            // if (localFile && localFile.childImageSharp && (mediaType != 'VIDEO')) {
-            //     return (
-            //         <div className='post' key={id}>
-            //             <a href={permalink}>
-            //                 <img 
-            //                     className='image'  
-            //                     src={localFile.childImageSharp.fluid.src}
-            //                     alt={caption}/>
-            //             </a>
-                        
-            //             {/* <div className='caption'>
-            //                 <p>{caption}</p>
-            //                 <a href={permalink} target='_blank' rel='noopener noreferrer'>
-            //                     View on Instagram
-            //                 </a>
-            //             </div> */}
-            //         </div>
-            //     );
-            // } else {
-            //     console.warn(`Skipping post with ID ${id} due to missing image data...`);
-            //     return null;
-            // }
-
-            return (
-                <div className='post' key={id}>
-                    <a href={permalink}>
-                        <img 
-                            className='image'  
-                            src={localFile.childImageSharp.fluid.src}
-                            alt={caption}/>
-                    </a>
-                </div>
-            );
-            })}
-        </Feed>
-      );
+    .caption {
+      padding: irem;
     }
   }
-
-
-// class Post extends React.Component {
-//   render() {
-//     const posts = this.props.data.allInstaNode.edges;
-
-//     console.log(posts);
-
-//     return (
-//       <Feed>
-//         {posts.map(({ node }) => {
-//             const { id, caption, permalink } = node;
-
-//             return (
-//                 <InstagramEmbed
-//                     key={id}
-//                     url={permalink}
-//                     hideCaption={false}
-//                     containerTagName='div'
-//                     protocol=''
-//                     injectScript
-//                     onLoading={() => {}}
-//                     onSuccess={() => {}}
-//                     onFailure={() => {}}
-//                 />
-//             );
-//         })}
-//       </Feed>
-//     );
-//   }
-// }
-
-// const Post = ({ data }) => {
-//   const [posts, setPosts] = useState([]);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const apiUrl = '/api/facebookProxy';
-
-//         // Extracting Instagram URLs from the GraphQL query result
-//         const instagramURLs = data.allInstaNode.edges.map(({ node }) => node.permalink);
-
-//         // Use Promise.all to concurrently fetch data from multiple URLs
-//         const fetchPromises = instagramURLs.map(url => {
-//           const params = {
-//             url,
-//             hidecaption: false,
-//             omitscript: true,
-//             fields: 'html',
-//           };
-//           const apiUrlWithParams = `${apiUrl}?${new URLSearchParams(params)}`;
-
-//           return fetch(apiUrlWithParams).then(response => response.json());
-//         });
-
-//         const results = await Promise.all(fetchPromises);
-
-//         // Assuming your results contain arrays of posts
-//         const allPosts = results.flatMap(result => result.posts || []);
-//         setPosts(allPosts);
-//         setLoading(false);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, [data]);
-
-//   if (loading) {
-//     return <p>Loading...</p>; // Display a loading indicator
-//   }
-
-//   return (
-//     <Feed>
-//       {posts.map(({ id, permalink }) => (
-//         <InstagramEmbed
-//           key={id}
-//           url={permalink}
-//           // url='https://www.instagram.com/p/Czys-kbOVt5/'
-//           hideCaption={false}
-//           containerTagName='div'
-//           protocol=''
-//           injectScript
-//           onLoading={() => {}}
-//           onSuccess={() => {}}
-//           onFailure={() => {}}
-//         />
-//       ))}
-//     </Feed>
-//   );
-// };
-
-export default Post;
+`;
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react";
+import { graphql } from "gatsby";
 
-import Loadable from 'react-loadable';
-import Post from '../components/sections/IGposts';
+import Loadable from "react-loadable";
+import Post from "../components/sections/IGposts";
 
-import Layout from '@common/Layout';
-import Navbar from '@common/Navbar';
-import Footer from '@sections/Footer';
+import Layout from "@common/Layout";
+import Navbar from "@common/Navbar";
+import Footer from "@sections/Footer";
 
 const Calendar = Loadable({
-    loader: () => import('../components/sections/Schedule'),
-    loading() {
-        return <div>Loading Calendar!</div>;
-    },
+  loader: () => import("../components/sections/Schedule"),
+  loading() {
+    return <div>Loading Calendar!</div>;
+  },
 });
 
-const OfficeHoursPage = ({ data }) => (
-    <Layout>
-        <Navbar />
-        <Calendar />
-        <Post data={data} />
-        <Footer />
-    </Layout>
+const EventsPage = ({ data }) => (
+  <Layout>
+    <Navbar />
+    <Calendar />
+    <Post />
+    <Footer />
+  </Layout>
 );
 
+<<<<<<< Updated upstream
 export const query = graphql`
     query {
         allInstaNode (
@@ -49,5 +50,32 @@ export const query = graphql`
         }
     }
 `;
+=======
+// export const query = graphql`
+//   query {
+//     allInstaNode(
+//       sort: { fields: timestamp, order: DESC }
+//       filter: { mediaType: { ne: "VIDEO" } }
+//     ) {
+//       edges {
+//         node {
+//           id
+//           permalink
+//           timestamp
+//           caption
+//           mediaType
+//           localFile {
+//             childImageSharp {
+//               fluid(maxWidth: 600) {
+//                 ...GatsbyImageSharpFluid
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
+>>>>>>> Stashed changes
 
-export default OfficeHoursPage;
+export default EventsPage;
